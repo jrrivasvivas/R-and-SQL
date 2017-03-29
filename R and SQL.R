abd9f3bf699b7f937e7278f864bddb820abecff4
jrrivasvivas <jrrivasvivas@gmail.com>
2017-03-29 00:08:16 -0300

    Add files via upload

--- R and SQL.R
+++ R and SQL.R
@@ -3,5 +3,20 @@ cn <- odbcDriverConnect(connection="Driver={SQL Server Native Client 11.0};
                         database=DataBase;
                         trusted_connection=yes;") 
 
+SELECT [NAME] FROM sysdatabases #listar esquemas
+SELECT table_name FROM Clientes.INFORMATION_SCHEMA.Tables #listar tablas
+
 query <- "select * from [DatabaseName].[dbo].[TableName]"
 
+cquery <- "select top 20 * from [DatabaseName].[dbo].[TableName]"
+
+data <- sqlQuery(cn,query)
+
+
+cn <- odbcDriverConnect(connection="Driver={SQL Server Native Client 11.0};
++                         server=localhost;
++                         database=Clientes;
++                         trusted_connection=yes;")
+query <- "select * from Clientes.[dbo].clientes$"
+cquery <- "select top 20 * from Clientes.[dbo].clientes$"
+data <- sqlQuery(cn,query)
